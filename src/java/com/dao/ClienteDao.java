a83e66de1f85a1605bb8697304271d154b7377a2
Miguel_R <samuraiequis@yahoo.com>
2019-02-11 21:13:00 -0600

    Creada Claseinterfaz ClienteDao.java

---
+++ src/java/com/dao/ClienteDao.java
@@ -0,0 +1,18 @@
+package com.dao;
+
+import com.model.Cliente;
+import java.util.List;
+
+public interface ClienteDao {
+    //Firmas de metodos
+    //Metodo que lista todas las instancias de Clientes
+    public List<Cliente> listarClientes();
+    //Metodo que agrega una nueva instancia de Cliente a la BD
+    public void insertCliente(Cliente cliente);
+    //Metodo que modifica una instancia de Cliente
+    public void updateCliente(Cliente cliente);
+    //metodo que elimina una instancia de Cliente
+    public void deleteCliente(Cliente cliente);
+    
+    
+}
---
+++ src/java/com/impl/ClienteDaoImp.java
@@ -0,0 +1,5 @@
+package com.impl;
+
+public class ClienteDaoImp {
+    
+}
